d138712bc3b139d40a0bacd076e5261a1661fc1e
rmmlr <elias.ruemmler@icloud.com>
2018-10-11 14:03:14 +0200

    Add HueHook-Lib and HueCmd

---
+++ HueCmd/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rca.HueCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Startup
+
+            var versionInfo = FileVersionInfo.GetVersionInfo(Assembly.GetEntryAssembly().Location);
+            var attribute = Assembly.GetExecutingAssembly()
+                    .GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false)
+                    .Cast<AssemblyDescriptionAttribute>().FirstOrDefault();
+
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            //Console.WriteLine("{0} v{1}", typeof(Program).Assembly.GetName().Name, typeof(Program).Assembly.GetName().Version);
+            Console.WriteLine("{0} v{1}", typeof(Program).Assembly.GetName().Name, versionInfo.ProductVersion);
+            Console.ResetColor();
+            if (attribute != null)
+                Console.WriteLine(attribute.Description);
+            Console.WriteLine();
+            Console.WriteLine(versionInfo.LegalCopyright);
+            for (int i = 0; i < 70; i++)
+                Console.Write("-");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            #endregion
+        }
+    }
+}
